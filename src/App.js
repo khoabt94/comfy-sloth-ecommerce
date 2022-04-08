d930516792ca3c3fe1fef60ba042a76377cb7764
khoabt94 <khoatienbui94@gmail.com>
2022-04-08 16:37:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  AboutPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  ProductsPage,
+  SingleProductPage,
+} from "./pages";
+
+function App() {
+  return (
+    <h4>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+
+          <Route exact path="/about">
+            <AboutPage />
+          </Route>
+
+          <Route exact path="/cart">
+            <CartPage />
+          </Route>
+
+          <Route exact path="/products">
+            <ProductsPage />
+          </Route>
+          <Route exact path="/products/:id" children={<SingleProductPage />} />
+          <Route exact path="/checkout">
+            <CheckoutPage />
+          </Route>
+          <Route path="*">
+            <ErrorPage />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </h4>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as HomePage } from "./HomePage";
+export { default as ProductsPage } from "./ProductsPage";
+export { default as SingleProductPage } from "./SingleProductPage";
+export { default as AboutPage } from "./AboutPage";
+export { default as CartPage } from "./CartPage";
+export { default as ErrorPage } from "./ErrorPage";
+export { default as CheckoutPage } from "./CheckoutPage";
+export { default as PrivateRoute } from "./PrivateRoute";
